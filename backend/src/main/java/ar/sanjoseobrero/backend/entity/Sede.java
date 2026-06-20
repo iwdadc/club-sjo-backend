5d9f2d80f461a7df05b5b5bebf55ec73f8834b31
Abigail <iwdadc@gmail.com>
2026-06-20 17:06:27 -0300

    entidades Actividad y Sede

---
+++ backend/src/main/java/ar/sanjoseobrero/backend/entity/Sede.java
@@ -0,0 +1,35 @@
+package ar.sanjoseobrero.backend.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "sedes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Sede {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "El nombre de la sede es obligatorio")
+    @Column(nullable = false, unique = true)
+    private String nombre;
+
+    @NotBlank(message = "La dirección es obligatoria")
+    @Column(nullable = false)
+    private String direccion;
+
+    // mappedBy indica que la relación se define del lado de Actividad
+   // @ManyToMany(mappedBy = "sedes", fetch = FetchType.LAZY)
+    //@Builder.Default
+    //private Set<Actividad> actividades = new HashSet<>();
+}
